2f86d6ba2ddd017d872f81197cd7ca08e4ca3d92
RajrupaDas <rajrupadas6@gamil.com>
2025-07-03 11:55:20 +0530

    Added LSTM model architecture scaffold

---
+++ src/lstm_model.py
@@ -0,0 +1,26 @@
+# lstm_model.py
+"""
+Build and train an LSTM model to predict satellite position (x, y, z) from past 5 time steps.
+"""
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.optimizers import Adam
+
+# Load preprocessed data
+X_train = np.load("../data/X_train.npy")
+y_train = np.load("../data/y_train.npy")
+X_test = np.load("../data/X_test.npy")
+y_test = np.load("../data/y_test.npy")
+
+# Model definition
+model = Sequential([
+    LSTM(64, return_sequences=True, input_shape=(5, 3)),
+    LSTM(32),
+    Dense(3)
+])
+
+model.compile(optimizer=Adam(learning_rate=0.001), loss='mse')
+model.summary()
+
